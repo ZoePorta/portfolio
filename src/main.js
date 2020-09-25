f4d890da4f7800d9e420b44c9e6e483bb4207277
ZoePorta <zoeportagarcia@gmail.com>
2020-09-25 10:25:26 +0200

    flipper individual

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+
+/* Font awesome icons */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faEye, faGithubAlt);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+/* /Font awesome icons */
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
+
   render: function(h) {
     return h(App);
   },
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,41 +1,77 @@
 <template>
-  <div class="work">
-    <flipper
-      class="test"
-      v-for="work in works"
-      :key="work.id"
-      width="21rem"
-      height="22.5rem"
-      :flipped="flipped"
-      @click="onClick"
-    >
-      <div class="card front" slot="front">
-        <h1>{{ work.title }}</h1>
-        <img src="../assets/crafty_phone.jpg" alt="" />
-      </div>
+  <div class="works">
+    <article v-for="work in works" :key="work.id" class="work">
+      <h1>{{ work.title }}</h1>
+      <flipper
+        class="card"
+        width="21rem"
+        height="22.5rem"
+        :flipped="work.flipped"
+        @click="work.flipped = !work.flipped"
+      >
+        <div class="cardside front" slot="front">
+          <img src="../assets/crafty_phone.jpg" alt="" />
+        </div>
 
-      <div class="card back" slot="back" v-html="work.desc">
-        {{ work.desc }}
-      </div>
-    </flipper>
+        <div class="cardside back" slot="back" v-html="work.desc">
+          {{ work.desc }}
+        </div>
+      </flipper>
+      <nav>
+        <a :href="work.github"
+          ><font-awesome-icon :icon="['fab', 'github-alt']"
+        /></a>
+        <a :href="work.link"><font-awesome-icon icon="eye"/></a>
+      </nav>
+    </article>
   </div>
 </template>
 
 <script>
 import Flipper from "vue-flipper";
+import card from "@/components/Card.vue";
 
 export default {
   name: "Work",
   components: { Flipper },
   data() {
     return {
-      flipped: false,
       works: [
         {
           title: "Crafty",
           image: "../assets/crafty_phone.jpg",
           desc: `Marketplace de productos artesanales. <br><br> Cuenta con buscador para filtrar por palabras clave y/o distintos parámetros simultánamente. 
           El back está hecho con Node.js y el front con Vue.js.`,
+          github: "http://github.com",
+          link: "google.com",
+          flipped: false,
+        },
+        {
+          title: "Crafty",
+          image: "../assets/crafty_phone.jpg",
+          desc: `Marketplace de productos artesanales. <br><br> Cuenta con buscador para filtrar por palabras clave y/o distintos parámetros simultánamente. 
+          El back está hecho con Node.js y el front con Vue.js.`,
+          github: "http://github.com",
+          link: "google.com",
+          flipped: false,
+        },
+        {
+          title: "Crafty",
+          image: "../assets/crafty_phone.jpg",
+          desc: `Marketplace de productos artesanales. <br><br> Cuenta con buscador para filtrar por palabras clave y/o distintos parámetros simultánamente. 
+          El back está hecho con Node.js y el front con Vue.js.`,
+          github: "http://github.com",
+          link: "google.com",
+          flipped: false,
+        },
+        {
+          title: "Crafty",
+          image: "../assets/crafty_phone.jpg",
+          desc: `Marketplace de productos artesanales. <br><br> Cuenta con buscador para filtrar por palabras clave y/o distintos parámetros simultánamente. 
+          El back está hecho con Node.js y el front con Vue.js.`,
+          github: "http://github.com",
+          link: "google.com",
+          flipped: false,
         },
       ],
     };
@@ -50,13 +86,16 @@ export default {
 
 <style src="vue-flipper/dist/vue-flipper.css"></style>
 <style scoped>
-.test {
+.work {
+  width: fit-content;
+}
+.card {
   overflow: hidden;
   border-radius: 1rem;
   box-shadow: 0 0 5px blue;
   background: blue;
 }
-.card {
+.cardside {
   height: 100%;
   width: 100%;
   overflow: hidden;
@@ -67,7 +106,7 @@ export default {
   background: hotpink;
 }
 
-.card img {
+.cardside img {
   position: fixed;
   top: 0;
   left: 0;
